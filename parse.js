385368e38767663ebe4bf2559fd696047afe2d4c
bramaning djoko susilo <bramaningds@hotmail.com>
2025-09-29 17:14:19 +0700

    update

--- parse.js
+++ parse.js
@@ -1,16 +1,13 @@
 const { Parser } = require("@dbml/core");
 const { readFileSync, readdirSync, writeFileSync } = require("fs");
 
-// const parsed = Parser.parseDBMLToJSONv2(readFileSync("./sales.dbml").toString());
-
-// console.info(JSON.stringify(parsed.tables[0], null, 4));
-
 readdirSync(__dirname)
 	.filter((filename) => filename.endsWith(".dbml"))
 	.forEach((filename) => {
         const json = Parser.parseDBMLToJSONv2(readFileSync(filename).toString());
 
-        console.info(json);
+        console.info(json.tables[0].fields);
 
-		writeFileSync(`./json/${filename}.json`, JSON.stringify(json, null, 4));
+		// writeFileSync(`./json/${filename}.json`, JSON.stringify(json, null, 4));
+		process.exit();
 	});
